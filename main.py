fb4387a9777b7f25c3715090958410e548222554
anshikashukla07 <anshikacs0012@gmail.com>
2020-03-24 02:19:36 +0530

    Changes to Output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 import pickle
 
@@ -6,12 +6,20 @@ file = open('model.pkl','rb')
 clf = pickle.load(file)
 file.close()
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def hello_world():
-    
-    # Code for inference
-    inputFeatures = [90, 0 , 25, 0, -1]
-    infProb = clf.predict_proba([inputFeatures])[0][1]
+    if(request.method == "POST"):
+        myDict = request.form
+        fever = float(myDict['fever'])
+        age = int(myDict['age'])
+        pain = int(myDict['pain'])
+        runnyNose = int(myDict['runnyNose'])
+        diffBreath = int(myDict['diffBreath'])
+        # Code for inference
+        inputFeatures = [fever, pain, age,  runnyNose, diffBreath]
+        infProb = clf.predict_proba([inputFeatures])[0][1]
+        print(infProb)
+        return render_template('show.html', inf=round(infProb*100))
 
     return render_template('index.html')
 
